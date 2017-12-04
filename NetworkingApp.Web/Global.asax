1921f7f83e614d8dd941ecb5024f53c230bc214e
Kevin Curnow <kevinscurnow@gmail.com>
2017-12-04 15:40:27 -0500

    Add project files.

---
+++ NetworkingApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetworkingApp.Web.MvcApplication" Language="C#" %>
---
+++ NetworkingApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetworkingApp.Web.Startup))]
+namespace NetworkingApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
