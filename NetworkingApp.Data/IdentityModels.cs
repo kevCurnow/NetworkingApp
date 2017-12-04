cfd865791bc4622faaff7d80db71efd329bad711
Kevin Curnow <kevinscurnow@gmail.com>
2017-12-04 16:49:13 -0500

    Edited Identity Model

--- NetworkingApp.Data/IdentityModels.cs
+++ NetworkingApp.Data/IdentityModels.cs
@@ -3,6 +3,9 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using NetworkingApp.Data;
 
 namespace NetworkingApp.Web.Models
 {
@@ -28,6 +31,36 @@ namespace NetworkingApp.Web.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<ConnectionEntity> Connections { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            modelBuilder
+            .Configurations
+            .Add(new IdentityUserLoginConfiguration())
+            .Add(new IdentityUserRoleConfiguration());
+
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
         }
     }
 }
